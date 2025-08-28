78e2faa6c7d7676a1c6232aaccba367a73e6d203
Julio Oliveira <oliveira.ti@live.com>
2025-08-28 12:34:41 -0300

    edit

---
+++ to-do-list/main.py
@@ -0,0 +1,6 @@
+def exibir_menu():
+    print("..::MENU DE TAREFAS::..\n 1 - Listar tarefas\n 2 - Adicionar tarefa\n 3 - Remover tarefa\n 4 - Sair")
+    print int(input("\n Digite a opção desejada."))
+
+
+exibir_menu

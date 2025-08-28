06043ad63b14e5334f7a27a15d8104f16c72c778
Julio Oliveira <oliveira.ti@live.com>
2025-08-28 16:03:32 -0300

    edit

--- to-do-list/gerenciador.py
+++ to-do-list/gerenciador.py
@@ -1,12 +1,29 @@
 import json
 
+
+
+
 def salvar_tarefas(tarefas):
     with open("tarefas.json", "w") as arquivo:
         json.dump(tarefas, arquivo)
 
-def carregar_tarefas(tarefas):
-    with open("tarefas.json", "r") as arquivo:
-        dados = json.load(arquivo)
+
+
+
+
+def carregar_tarefas():
+    try:
+        with open("tarefas.json", "r") as arquivo:
+            tarefas = json.load(arquivo)
+    except FileNotFoundError:
+        tarefas = []
+
+    return tarefas
+    
+
+
+
+
 
 def adicionar_tarefa(tarefas, descricao_nova_tarefa):
     nova_tarefa = {

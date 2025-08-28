6075918af4c62d09d0eebb9cea1669d28b83fd4e
Julio Oliveira <oliveira.ti@live.com>
2025-08-28 12:15:53 -0300

    edit

--- to-do-list/gerenciador.py
+++ to-do-list/gerenciador.py
@@ -1,1 +1,18 @@
-print("Olá")
+import json
+
+def salvar_tarefas(tarefas):
+    with open("tarefas.json", "w") as arquivo:
+        json.dump(tarefas, arquivo)
+
+def carregar_tarefas(tarefas):
+    with open("tarefas.json", "r") as arquivo:
+        dados = json.load(arquivo)
+
+def adicionar_tarefa(tarefas, descricao_nova_tarefa):
+    nova_tarefa = {
+        "descrição": descricao_nova_tarefa,
+        "status": "pendente"
+    }
+
+    tarefas.append(nova_tarefa)
+    print(f"Tarefa '{descricao_nova_tarefa}' adicionada com sucesso!")

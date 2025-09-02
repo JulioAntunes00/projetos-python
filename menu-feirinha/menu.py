d539b1ffd4e923bbcebef33879dbf8f773e905c6
Julio Oliveira <oliveira.ti@live.com>
2025-09-02 12:22:52 -0300

    Update menu.py

--- menu-feirinha/menu.py
+++ menu-feirinha/menu.py
@@ -1,30 +1,40 @@
-print("LISTA DE FRUTAS")
+print("\n..:LISTA DE FRUTAS:..")
 
 lista = ["Uva", "Banana", "Melancia", "Abacate"]
 
-#def menu():
-  #menu = input("Escolha uma opção:\n 1. Adicionar \n 2. Remover\n 3. Listar\n 4. Ordenar\n 0. Sair\n")
+def menu():
+  print("""
+1. Adicionar
+2. Remover
+3. Listar
+4. Ordenar
+0. Sair
+""")
+
+
 while True:
-  menu = input("Escolha uma opção:\n 1. Adicionar \n 2. Remover\n 3. Listar\n 4. Ordenar\n 0. Sair\n")
-  if menu == "1":
-    adicionar = input("Digite a fruta para adicionar")
-    lista.append(adicionar)
-    print(lista)
-  elif menu == "2":
-    remover = input("Qual fruta deseja remover?")
-    if remover in lista:
-      lista.remove(remover)
-      print(lista)
+    menu()
+    opcao = input("Digite uma opção: ")
+
+    if opcao == "1":
+        adicionar = input("Digite a fruta para adicionar: ").capitalize()
+        lista.append(adicionar)
+        print(lista)
+
+    elif opcao == "2":
+        remover = input("Qual fruta deseja remover?").capitalize()
+        if remover in lista:
+            lista.remove(remover)
+            print(lista)
         
-  elif menu == "3":
-    print(lista)
+    elif opcao == "3":
+        print(lista)
     
-  elif menu == "4":
-    lista.sort()
-    print("Lista ordenada:", lista)
+    elif opcao == "4":
+        lista.sort()
+        print("Lista ordenada:", lista)
       
-  elif menu == "0":
-    break
-  else:
-    print("Opção inválida")
-    menu
+    elif opcao == "0":
+        break
+    else:
+        print("Opção inválida")
